b75d21ace7836686886f1cf1abc10a19ec861c2b
Doggo785 <stephane.plathey@proton.me>
2025-10-17 10:52:11 +0200

    Add initial project setup and core functionality
    Updated .gitignore to exclude Visual Studio and .NET files.
    Added Paradigme.sln to define the solution structure.
    Created Paradigme.csproj targeting .NET 8.0 with executable output.
    Implemented Program.cs with `AgeCheck` and `AverageGrade` classes.
    Included basic functionality for age verification and grade averaging.

---
+++ CSharp/Paradigme/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+main();
+
+static void main()
+{
+    // AgeCheck.Check();
+    //AverageGrade avgGrade = new AverageGrade();
+    //avgGrade.AddGrade(100);
+    //avgGrade.AddGrade(0);
+    //Console.WriteLine($"Average Grade: {avgGrade.CalculateAverage()}");
+}
+class AgeCheck
+{
+    static public void Check()
+    {
+        Console.WriteLine("Age Verification");
+        Console.WriteLine("----------------");
+        Console.WriteLine("Enter your name");
+        string rawname = Console.ReadLine() ?? "";
+        string name = string.IsNullOrWhiteSpace(rawname) ? "Doggo" : rawname;
+        Console.WriteLine("Enter your age:");
+        int input = int.Parse(Console.ReadLine() ?? "1");
+        Console.WriteLine($"Welcome {name} !");
+        if (input < 18)
+        {
+            Console.WriteLine("You are a minor.");
+        }
+        else
+        {
+            Console.WriteLine("You are major.");
+        }
+    }
+}
+
+class AverageGrade()
+{
+    private List<int> grades = new List<int>();
+    public void AddGrade(int grade)
+    {
+        grades.Add(grade);
+    }
+    public double CalculateAverage()
+    {
+        if (grades.Count == 0) return 0;
+        return grades.Average();
+    }
+}
